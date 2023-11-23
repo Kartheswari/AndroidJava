d492bed847ce42543d1fc5b387e7602248b0fb5b
Kartheswari <151842443+Kartheswari@users.noreply.github.com>
2023-11-23 19:13:38 +0530

    Update Encapsulation.java

--- Encapsulation.java
+++ Encapsulation.java
@@ -1,31 +1,39 @@
 //Encapsulation Concept
 
-//a way to restrict the direct access to some components of an object
-// getters and setters
-public class Encapsulation{
+//A way to restrict the direct access to some components of an object
+
+// To access the restrict methods we can use getters and setters
+
+public class Encapsulation
+{
     public static void main(String[] args) {
         System.out.println("main method");
         Employee obj = new Employee("HR2");
+        
         obj.setEmployeeId("18880");
 
         System.out.println("getEmployeeId---->"+obj.getEmployeeId());
     }
 }
 
-
 class Employee
 {
     private String employeeId;
     String employeeRole;
     private String name;
-    public String getEmployeeId() {
+    
+    public String getEmployeeId() 
+    {
         return employeeId;
     }
+    
     Employee(String empRole)
     {
         employeeRole = empRole;
     }
-    public void setEmployeeId(String empId) {
+    
+    public void setEmployeeId(String empId) 
+    {
         if(hasPermission(employeeRole))
         {
             this.employeeId = empId;
@@ -35,6 +43,7 @@ class Employee
             System.out.println("You don't have permission");
         }
     }
+    
     private boolean hasPermission(String empRole)
     {
         return empRole.equals("HR002");
