6e252f141b531ec371f290a241b13dbc9f98d3ce
Kartheswari <151842443+Kartheswari@users.noreply.github.com>
2023-11-23 19:19:34 +0530

    Update PrimitiveNonPrimitive.java

--- PrimitiveNonPrimitive.java
+++ PrimitiveNonPrimitive.java
@@ -1,40 +1,55 @@
 //Primitive -  Non primitive
 
-//primitive data types include boolean, char, byte, short, int, long, float and double
-//memory size for primitive types is fixed,
-//default value 0 for int, false for boolean
+//Primitive data types include boolean, char, byte, short, int, long, float and double
+
+//Memory size for primitive types is fixed
+
+//Default value 0 for int, false for boolean.... etc
 
 //Non primitive data types Array, String, Class, Object, Long, Boolean
+
 //Non-primitive types can be used to call methods
-//their memory usage can vary depending on the size of the object
-//default value is null for all
-public class PrimitiveNonPrimitive{
-    public static void main(String[] args) {
+
+//Non-primitive types memory usage can vary depending on the size of the object
+
+//Non-primitive default value is null for all objects
+
+public class PrimitiveNonPrimitive
+{
+    public static void main(String[] args) 
+    {
         System.out.println("main method");
+        
         TeamMembers obj = new TeamMembers();
         System.out.println("floatData---->"+obj.floatData+"-----intData----->"+obj.intData+"----booleanData---->"+obj.booleanData);
+        
         System.out.println("Non primitive-->"+obj.aBoolean);
     }
 }
 
-class Employee{
-    Employee() {
+class Employee
+{
+    Employee() 
+    {
 
     }
 }
 
-class TeamMembers extends Employee{
+class TeamMembers extends Employee
+{
     boolean booleanData;
     int intData;
     float floatData;
 
     Boolean aBoolean; //Non-primitive
+  
     TeamMembers()
     {
         aBoolean = false;
         booleanData = true;
         intData = 1666;
         floatData = 1.5F;
+        
         System.out.println("This is TeamMembers constructor ---->");
     }
 }
