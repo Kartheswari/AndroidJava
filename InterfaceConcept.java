9cccdd532eb66ed494c0c65903232d0f9a758af2
Kartheswari <151842443+Kartheswari@users.noreply.github.com>
2023-11-23 19:16:54 +0530

    Update InterfaceConcept.java

--- InterfaceConcept.java
+++ InterfaceConcept.java
@@ -1,57 +1,77 @@
 //Interface Concepts
-//Since Java 8, we can have default and static methods in an interface.
-//datamembers -> public, static and final by default
-// Constructotr Not allowed in interface -
-// Constructors are used to initialize instances of a class, but interfaces do not have instances, so there's no need for constructors in interfaces.
-//interface supports multiple inheritance but class not supported.
-//multiple inheritance is not supported in class because of ambiguity but in interface implementation is provided by the implementation class.
 
-public class InterfaceConcept{
+//Since Java 8, we can have default and static methods in an interface.
+
+//Datamembers -> public, static and final by default
+
+//Constructotr Not allowed in interface -
+
+//Constructors are used to initialize instances of a class, but interfaces do not have instances, so there's no need for constructors in interfaces.
+
+//Interface supports multiple inheritance but class not supported.
+
+//Multiple inheritance is not supported in class because of ambiguity but in interface implementation is provided by the implementation class.
+
+public class InterfaceConcept
+{
     public static void main(String[] args) {
         System.out.println("main method");
+        
         TeamMembers obj = new TeamMembers();
         obj.empId();
     }
 }
-interface Company {
+
+interface Company 
+{
     abstract void companyName();
 }
+
 interface Employee{
 
-//    Employee()
-//    {}
-
     int empId = 1666;
+//    Employee()
+//    {
+    
+//    }
 
     abstract void name();
 
     abstract void empId();
+    
     //abstract void mobileNumber();
-    static void isEligibleMeternityLeave(String gender) {
+    
+    static void isEligibleForMeternityLeave(String gender) 
+    {
         System.out.println("isEligibleMeternityLeave  ---->");
     }
+    
     default void annualLeaveDays()
     {
         System.out.println("annualLeaveDays  ---->");
     }
 }
 
-class TeamMembers implements Employee,Company{
+class TeamMembers implements Employee,Company
+{
     TeamMembers()
     {
         System.out.println("This is TeamMembers constructor ---->");
     }
+    
     public void name ()
     {
         System.out.println("Namee  ---->");
     }
+    
     public void empId()
     {
         System.out.println("empId  ---->"+empId);
     }
 
     @Override
-    public void companyName() {
+    public void companyName() 
+    {
         System.out.println("companyName  ---->");
     }
 }
